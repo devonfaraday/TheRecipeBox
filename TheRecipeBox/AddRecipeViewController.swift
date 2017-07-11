a39173bf17aeb9e6437a19ae58f08ae35d3e713d
devonfaraday <christianmcmullin@gmail.com>
2017-07-11 11:45:59 -0600

    Added a function to resize images so they images are no longer massive files.  Speed imporved

--- TheRecipeBox/AddRecipeViewController.swift
+++ TheRecipeBox/AddRecipeViewController.swift
@@ -208,7 +208,8 @@ class AddRecipeViewController: UIViewController, UITableViewDataSource, UITextFi
                 let cookTime = cookTimeTextField.text
                 else { return }
             if let image = recipeImageView.image {
-                let imageData = UIImageJPEGRepresentation(image, 0.5)
+                let img = ImageResizer.resizeImage(image: image, targetSize: CGSize(width: 414, height: 200))
+                let imageData = UIImageJPEGRepresentation(img, 1.0)
                 let recipe = Recipe(name: name, prepTime: prepTime, servingSize: servings, cookTime: cookTime, recipeImageData: imageData)
                 
                 RecipeController.shared.addRecipeToCloudKit(recipe: recipe, ingredients: ingredients, instructions: instructions, completion: { (error) in
---
+++ TheRecipeBox/StackOverflow solutions.swift
@@ -0,0 +1,12 @@
+//
+//  StackOverflow solutions.swift
+//  TheRecipeBox
+//
+//  Created by Christian McMullin on 7/11/17.
+//  Copyright © 2017 Christian McMullin. All rights reserved.
+//
+
+import Foundation
+
+
+
